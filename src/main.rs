ee7573b96179089398786ed6d82c3e5e1687433d
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2019-03-03 03:49:59 -0500

    no-std boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    main();
+}
+
+#[no_mangle]
+pub extern "C" fn mainCRTStartup() -> ! {
+    main();
+}
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
 }
