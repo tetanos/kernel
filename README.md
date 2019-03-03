f911428d75d23cd222c7dad6696a867f858434fd
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2019-03-03 05:09:29 -0500

    bootable hello world

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # TetanOS
 An attempt at building an operating system, make sure you're vaxxed.
+
+## Build
+
+
+Make sure bootimage is installed, it compile the bootloader and prepend it to our kernel
+
+```sh
+cargo install bootimage
+```
+
+
+Build the bootable binary
+
+```sh
+bootimage build
+```
+
+
+Run the binary using qemu
+
+```sh
+qemu-system-x86_64 -drive format=raw,file=target/x86_64/debug/bootimage-tetan_os.bin
+```
+
+
+## License
+
+TetanOS is MIT licensed.
+
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hi! This is TetanOS. be careful it's kinda rusty in here";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    main();
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xc;
+        }
+    }
+
+    loop {}
 }
 
